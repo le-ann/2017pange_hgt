4c3322217a697efdd5597d1ae07244b483a75e5d
Ann Le <ann@info114.(none)>
2017-09-08 17:17:55 -0400

    Added bash code to extract accession numbers from gbk files

---
+++ scripts/extract_accession_from_gbk.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------------
+# Extracts the accession numbers for a particular list of gbk files from the respective
+# gbk files.
+# $1 = path to directory with all the run list inputs
+# Note: the output directory points to inputfiles, and should be created before the code
+# is executed 
+#--------------------------------------------------------------------------------------
+
+for y in {1..100}
+	do 
+		for x in `cat $1/run$y`
+			do
+				grep -oP '(?<=ACCESSION)\s{3}[A-Z]{2}\d{6}' $x >> inputfiles/accessioninput$y
+			done
+	done 
+	 
